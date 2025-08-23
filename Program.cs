d4c3fd14f549dd99f9569d95aac9d16301d44923
henrique <gerassi.henrique07@gmail.com>
2025-08-23 11:47:57 -0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GeradorDeSenhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gerador de Senhas");
+            Console.WriteLine("Quantos caracteres a senha deve ter?");
+            int tamanho = int.Parse(Console.ReadLine());
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+            Random random = new Random();
+            char[] senha = new char[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                senha[i] = caracteres[random.Next(caracteres.Length)];
+            }
+
+            Console.WriteLine("Senha gerada: " + new string(senha));
+        }
+    }
+}
